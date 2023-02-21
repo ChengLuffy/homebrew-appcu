785f42a1c77ffc8b8f24a9082551a0a28d8082ba
chenluffy <chengluffy@hotmail.com>
2023-02-21 15:45:19 +0800

    temp 降低版本到 v0.1.0 测试 cd 流程

--- Formula/appcu.rb
+++ Formula/appcu.rb
@@ -1,12 +1,12 @@
 class Appcu < Formula
   desc "一个用于检查 macOS 应用更新的 CLI 工具"
   homepage "https://github.com/ChengLuffy/application_check_update"
-  url "https://github.com/ChengLuffy/application_check_update/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "88b6701048742db0cd6fd7185876d38e16b647d9182b3123311e2b2fcfa0f082"
+  url "https://github.com/ChengLuffy/application_check_update/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "88cee4be7f33c09468467e8fc311cbbb85756b6a94e53c02410b824223a62ca8"
   license "MIT"
 
   bottle do
-    root_url "https://github.com/ChengLuffy/homebrew-appcu/releases/download/appcu-0.1.1"
+    root_url "https://github.com/ChengLuffy/homebrew-appcu/releases/download/appcu-0.1.0"
     sha256 cellar: :any_skip_relocation, monterey: "19aa8180ee2b5e54794a641a51cb642d878da8f1a70493b47876eece90ec65ad"
   end
 
