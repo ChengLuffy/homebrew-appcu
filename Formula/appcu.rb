a8ac6b6a08b3f7500cd698103b862bad7f6d7711
成璐飞 <chengluffy@gmail.com>
2023-02-21 09:21:05 +0800

    appcu 0.1.1 (new formula)

---
+++ Formula/appcu.rb
@@ -0,0 +1,19 @@
+class Appcu < Formula
+  desc "一个用于检查 macOS 应用更新的 CLI 工具"
+  homepage "https://github.com/ChengLuffy/application_check_update"
+  url "https://github.com/ChengLuffy/application_check_update/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "88b6701048742db0cd6fd7185876d38e16b647d9182b3123311e2b2fcfa0f082"
+  license "MIT"
+
+  depends_on "rust" => :build
+  depends_on :macos
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    system bin/"appcu", "generate_config"
+    assert_predicate testpath/".config/appcu/config.yaml", :exist?
+  end
+end
