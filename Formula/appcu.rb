0f81e055bc24cc50b5bbbce6099ce3e02d21a678
chenluffy <chengluffy@hotmail.com>
2023-03-15 14:18:51 +0800

    appcu 0.1.5
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/appcu.rb
+++ Formula/appcu.rb
@@ -1,8 +1,8 @@
 class Appcu < Formula
   desc "一个用于检查 macOS 应用更新的 CLI 工具"
   homepage "https://github.com/ChengLuffy/application_check_update"
-  url "https://github.com/ChengLuffy/application_check_update/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "9dde2328b932ddb16ea376ea135ebc7521a10352577f17d7c37c686895be2f69"
+  url "https://github.com/ChengLuffy/application_check_update/archive/refs/tags/v0.1.5.tar.gz"
+  sha256 "f64d665f630d317edf972619011cc0e9c93ab3ecd8b741243cc6b2e0b9d3f308"
   license "MIT"
 
   bottle do
